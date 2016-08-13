8c42ae1183fd95ee92f1a16934e1422b122f2f19
一庭霜月 <316193839@qq.com>
2016-08-13 15:15:19 +0800

    第343行加个true吧
    json_decode的结果应该是数组才对

--- emchat-server-php/Easemob.class.php
+++ emchat-server-php/Easemob.class.php
@@ -340,7 +340,7 @@ class Easemob {
 			$arr = unserialize ( fgets ( $fp ) );
 			if ($arr ['expires_in'] < time ()) {
 				$result = $this->postCurl ( $url, $option, $head = 0 );
-				$result = json_decode($result);
+				$result = json_decode($result，true);
 				$result ['expires_in'] = $result ['expires_in'] + time ();
 				@fwrite ( $fp, serialize ( $result ) );
 				return $result ['access_token'];
